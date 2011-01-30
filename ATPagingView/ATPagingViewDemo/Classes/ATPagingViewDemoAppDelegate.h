dffccee676a712fb245ad9b11c22a1b0b9b81d20
Andrey Tarantsov <andreyvit@gmail.com>
2011-01-30 16:36:19 +0600

    Create ATPagingViewDemo as a window-based iPhone project

---
+++ ATPagingView/ATPagingViewDemo/Classes/ATPagingViewDemoAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  Copyright 2011 Andrey Tarantsov. Distributed under the MIT license.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATPagingViewDemoAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ ATPagingView/ATPagingViewDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  Copyright 2011 Andrey Tarantsov. Distributed under the MIT license.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
