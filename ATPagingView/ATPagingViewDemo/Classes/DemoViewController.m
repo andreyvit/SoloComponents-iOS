9fdc412a721549cfc684cd944914ee83e230035c
Andrey Tarantsov <andreyvit@gmail.com>
2011-01-31 00:49:52 +0600

    ATPagingView: remove unused demo controller methods

--- ATPagingView/ATPagingViewDemo/Classes/DemoViewController.m
+++ ATPagingView/ATPagingViewDemo/Classes/DemoViewController.m
@@ -8,24 +8,8 @@
 
 @implementation DemoViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return YES;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-}
-
-- (void)viewDidUnload {
-    [super viewDidUnload];
-}
-
-- (void)dealloc {
-    [super dealloc];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
