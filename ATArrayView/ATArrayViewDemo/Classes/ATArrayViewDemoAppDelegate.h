40e93bbb93ed81f362f04d81e3c58c0210a063b1
Andrey Tarantsov <andreyvit@gmail.com>
2011-01-31 22:20:43 +0600

    ATArrayView: new demo project created

---
+++ ATArrayView/ATArrayViewDemo/Classes/ATArrayViewDemoAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  Copyright 2011 Andrey Tarantsov. Distributed under the MIT license.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATArrayViewDemoAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ ATArrayView/ATArrayViewDemo/Classes/ATArrayViewDemoAppDelegate.m
@@ -0,0 +1,54 @@
+//
+//  Copyright 2011 Andrey Tarantsov. Distributed under the MIT license.
+//
+
+#import "ATArrayViewDemoAppDelegate.h"
+
+@implementation ATArrayViewDemoAppDelegate
+
+@synthesize window;
+
+
+#pragma mark -
+#pragma mark Application lifecycle
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+}
+
+
+- (void)dealloc {
+    [window release];
+    [super dealloc];
+}
+
+
+@end
