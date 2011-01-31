6779c4c1923d898e001fef386cba437346f8d767
Andrey Tarantsov <andreyvit@gmail.com>
2011-01-31 22:29:52 +0600

    ATArrayView: demo app skeleton

---
+++ ATArrayView/ATArrayViewDemo/Classes/DemoViewController.h
@@ -0,0 +1,12 @@
+//
+//  Copyright 2011 Andrey Tarantsov. Distributed under the MIT license.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DemoViewController : UIViewController {
+
+}
+
+@end
---
+++ ATArrayView/ATArrayViewDemo/Classes/DemoViewController.m
@@ -0,0 +1,15 @@
+//
+//  Copyright 2011 Andrey Tarantsov. Distributed under the MIT license.
+//
+
+#import "DemoViewController.h"
+
+
+@implementation DemoViewController
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
+}
+
+
+@end
